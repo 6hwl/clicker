374ca19cc4399b1acaa9de1b05fc155af7e56596
Minxin Gao <mgao@umass.edu>
2016-08-29 14:17:24 -0400

    Try/Catch Added for Load

---
+++ Clicker/Clicker/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Clicker
+{
+    partial class Clicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clicker));
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.numClickLabel = new System.Windows.Forms.Label();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.currentLabel = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.autoClickButton = new System.Windows.Forms.Button();
+            this.perClickLabel = new System.Windows.Forms.Label();
+            this.upgradeClickLabel = new System.Windows.Forms.Label();
+            this.currentAutoClick = new System.Windows.Forms.Label();
+            this.upgradeCostAuto = new System.Windows.Forms.Label();
+            this.clickerTimer = new System.Windows.Forms.Timer(this.components);
+            this.autoClickButton2 = new System.Windows.Forms.Button();
+            this.autoClick2Current = new System.Windows.Forms.Label();
+            this.autoClick2Cost = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.dummyButton = new System.Windows.Forms.Button();
+            this.Auto3Cost = new System.Windows.Forms.Label();
+            this.Auto3Current = new System.Windows.Forms.Label();
+            this.Auto3Button = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clickMeButton.Location = new System.Drawing.Point(12, 49);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(315, 61);
+            this.clickMeButton.TabIndex = 0;
+            this.clickMeButton.TabStop = false;
+            this.clickMeButton.Text = "Click Me";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.clickMeButton_Click);
+            // 
+            // numClickLabel
+            // 
+            this.numClickLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numClickLabel.Location = new System.Drawing.Point(12, 9);
+            this.numClickLabel.Name = "numClickLabel";
+            this.numClickLabel.Size = new System.Drawing.Size(315, 37);
+            this.numClickLabel.TabIndex = 1;
+            this.numClickLabel.Text = "0";
+            this.numClickLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.upgradeButton.Location = new System.Drawing.Point(12, 137);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(125, 49);
+            this.upgradeButton.TabIndex = 2;
+            this.upgradeButton.TabStop = false;
+            this.upgradeButton.Text = "Upgrade Click Amount";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            this.upgradeButton.Click += new System.EventHandler(this.upgradeButton_Click);
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Location = new System.Drawing.Point(167, 121);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(41, 13);
+            this.currentLabel.TabIndex = 3;
+            this.currentLabel.Text = "Current";
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Location = new System.Drawing.Point(251, 121);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(28, 13);
+            this.costLabel.TabIndex = 4;
+            this.costLabel.Text = "Cost";
+            // 
+            // autoClickButton
+            // 
+            this.autoClickButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.autoClickButton.Location = new System.Drawing.Point(12, 192);
+            this.autoClickButton.Name = "autoClickButton";
+            this.autoClickButton.Size = new System.Drawing.Size(125, 49);
+            this.autoClickButton.TabIndex = 5;
+            this.autoClickButton.TabStop = false;
+            this.autoClickButton.Text = "Buy AutoClick 1";
+            this.autoClickButton.UseVisualStyleBackColor = true;
+            this.autoClickButton.Click += new System.EventHandler(this.autoClickButton_Click);
+            // 
+            // perClickLabel
+            // 
+            this.perClickLabel.AutoSize = true;
+            this.perClickLabel.Location = new System.Drawing.Point(179, 155);
+            this.perClickLabel.Name = "perClickLabel";
+            this.perClickLabel.Size = new System.Drawing.Size(13, 13);
+            this.perClickLabel.TabIndex = 7;
+            this.perClickLabel.Text = "1";
+            this.perClickLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // upgradeClickLabel
+            // 
+            this.upgradeClickLabel.AutoSize = true;
+            this.upgradeClickLabel.Location = new System.Drawing.Point(251, 155);
+            this.upgradeClickLabel.Name = "upgradeClickLabel";
+            this.upgradeClickLabel.Size = new System.Drawing.Size(19, 13);
+            this.upgradeClickLabel.TabIndex = 8;
+            this.upgradeClickLabel.Text = "20";
+            this.upgradeClickLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // currentAutoClick
+            // 
+            this.currentAutoClick.AutoSize = true;
+            this.currentAutoClick.Location = new System.Drawing.Point(179, 210);
+            this.currentAutoClick.Name = "currentAutoClick";
+            this.currentAutoClick.Size = new System.Drawing.Size(13, 13);
+            this.currentAutoClick.TabIndex = 9;
+            this.currentAutoClick.Text = "0";
+            this.currentAutoClick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // upgradeCostAuto
+            // 
+            this.upgradeCostAuto.AutoSize = true;
+            this.upgradeCostAuto.Location = new System.Drawing.Point(251, 210);
+            this.upgradeCostAuto.Name = "upgradeCostAuto";
+            this.upgradeCostAuto.Size = new System.Drawing.Size(25, 13);
+            this.upgradeCostAuto.TabIndex = 10;
+            this.upgradeCostAuto.Text = "100";
+            this.upgradeCostAuto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clickerTimer
+            // 
+            this.clickerTimer.Enabled = true;
+            this.clickerTimer.Tick += new System.EventHandler(this.clickerTimer_Tick);
+            // 
+            // autoClickButton2
+            // 
+            this.autoClickButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.autoClickButton2.Location = new System.Drawing.Point(12, 247);
+            this.autoClickButton2.Name = "autoClickButton2";
+            this.autoClickButton2.Size = new System.Drawing.Size(125, 49);
+            this.autoClickButton2.TabIndex = 11;
+            this.autoClickButton2.TabStop = false;
+            this.autoClickButton2.Text = "Buy AutoClick 100";
+            this.autoClickButton2.UseVisualStyleBackColor = true;
+            this.autoClickButton2.Click += new System.EventHandler(this.autoClickButton2_Click);
+            // 
+            // autoClick2Current
+            // 
+            this.autoClick2Current.AutoSize = true;
+            this.autoClick2Current.Location = new System.Drawing.Point(179, 265);
+            this.autoClick2Current.Name = "autoClick2Current";
+            this.autoClick2Current.Size = new System.Drawing.Size(13, 13);
+            this.autoClick2Current.TabIndex = 12;
+            this.autoClick2Current.Text = "0";
+            this.autoClick2Current.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // autoClick2Cost
+            // 
+            this.autoClick2Cost.AutoSize = true;
+            this.autoClick2Cost.Location = new System.Drawing.Point(251, 265);
+            this.autoClick2Cost.Name = "autoClick2Cost";
+            this.autoClick2Cost.Size = new System.Drawing.Size(37, 13);
+            this.autoClick2Cost.TabIndex = 13;
+            this.autoClick2Cost.Text = "10000";
+            this.autoClick2Cost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveButton.Location = new System.Drawing.Point(12, 363);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.TabStop = false;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loadButton.Location = new System.Drawing.Point(133, 363);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 15;
+            this.loadButton.TabStop = false;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // dummyButton
+            // 
+            this.dummyButton.Location = new System.Drawing.Point(197, 204);
+            this.dummyButton.Name = "dummyButton";
+            this.dummyButton.Size = new System.Drawing.Size(0, 0);
+            this.dummyButton.TabIndex = 16;
+            this.dummyButton.UseVisualStyleBackColor = true;
+            // 
+            // Auto3Cost
+            // 
+            this.Auto3Cost.AutoSize = true;
+            this.Auto3Cost.Location = new System.Drawing.Point(251, 320);
+            this.Auto3Cost.Name = "Auto3Cost";
+            this.Auto3Cost.Size = new System.Drawing.Size(55, 13);
+            this.Auto3Cost.TabIndex = 19;
+            this.Auto3Cost.Text = "10000000";
+            this.Auto3Cost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Auto3Current
+            // 
+            this.Auto3Current.AutoSize = true;
+            this.Auto3Current.Location = new System.Drawing.Point(179, 320);
+            this.Auto3Current.Name = "Auto3Current";
+            this.Auto3Current.Size = new System.Drawing.Size(13, 13);
+            this.Auto3Current.TabIndex = 18;
+            this.Auto3Current.Text = "0";
+            this.Auto3Current.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Auto3Button
+            // 
+            this.Auto3Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Auto3Button.Location = new System.Drawing.Point(12, 302);
+            this.Auto3Button.Name = "Auto3Button";
+            this.Auto3Button.Size = new System.Drawing.Size(125, 49);
+            this.Auto3Button.TabIndex = 17;
+            this.Auto3Button.TabStop = false;
+            this.Auto3Button.Text = "Buy AutoClick 10000";
+            this.Auto3Button.UseVisualStyleBackColor = true;
+            this.Auto3Button.Click += new System.EventHandler(this.Auto3Button_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(252, 363);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 20;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Clicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 398);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.Auto3Cost);
+            this.Controls.Add(this.Auto3Current);
+            this.Controls.Add(this.Auto3Button);
+            this.Controls.Add(this.dummyButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.autoClick2Cost);
+            this.Controls.Add(this.autoClick2Current);
+            this.Controls.Add(this.autoClickButton2);
+            this.Controls.Add(this.upgradeCostAuto);
+            this.Controls.Add(this.currentAutoClick);
+            this.Controls.Add(this.upgradeClickLabel);
+            this.Controls.Add(this.perClickLabel);
+            this.Controls.Add(this.autoClickButton);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.currentLabel);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.numClickLabel);
+            this.Controls.Add(this.clickMeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(355, 436);
+            this.MinimumSize = new System.Drawing.Size(355, 436);
+            this.Name = "Clicker";
+            this.Text = " Click Me";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Label numClickLabel;
+        private System.Windows.Forms.Button upgradeButton;
+        private System.Windows.Forms.Label currentLabel;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Button autoClickButton;
+        private System.Windows.Forms.Label upgradeClickLabel;
+        private System.Windows.Forms.Label currentAutoClick;
+        private System.Windows.Forms.Label upgradeCostAuto;
+        private System.Windows.Forms.Label perClickLabel;
+        private System.Windows.Forms.Timer clickerTimer;
+        private System.Windows.Forms.Button autoClickButton2;
+        private System.Windows.Forms.Label autoClick2Current;
+        private System.Windows.Forms.Label autoClick2Cost;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button dummyButton;
+        private System.Windows.Forms.Label Auto3Cost;
+        private System.Windows.Forms.Label Auto3Current;
+        private System.Windows.Forms.Button Auto3Button;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Clicker/Clicker/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Clicker
+{
+    public partial class Clicker : Form
+    {
+        //Initialize Variables
+        long perClick, num, upgradeCost, upgradeAutoCost, currentAuto, currentAuto2, currentAuto2Cost, currentAuto1;
+        long auto3Cost, auto3Current;
+        public Clicker()
+        {
+            //Set each variables with label's text
+            InitializeComponent();
+            perClick = long.Parse(perClickLabel.Text);
+            num = long.Parse(numClickLabel.Text);
+            upgradeCost = long.Parse(upgradeClickLabel.Text);
+            upgradeAutoCost = long.Parse(upgradeCostAuto.Text);
+            currentAuto = long.Parse(currentAutoClick.Text);
+            currentAuto2 = long.Parse(autoClick2Current.Text);
+            currentAuto2Cost = long.Parse(autoClick2Cost.Text);
+            auto3Cost = long.Parse(Auto3Cost.Text);
+            auto3Current = long.Parse(Auto3Current.Text);
+            currentAuto1 = 0;
+        }
+
+        private void clickMeButton_Click(object sender, EventArgs e)
+        {
+            //dummyButton is for preventing user from clicking by just holding enter key.
+            dummyButton.Focus();
+            //When clicked Number of Clicks raise by perClick value.
+            num += perClick;
+            numClickLabel.Text = num.ToString();
+        }
+
+        private void upgradeButton_Click(object sender, EventArgs e)
+        {
+            /*Upgrading the value by checking if you have enough clicks.
+             *This is done by decrement of number of clicks by the cost.
+             *Updating the cost of upgrade.
+             *Updating the value by a fixed number.
+             *Updating all text labels.
+            */
+            if (num >= upgradeCost)
+            {
+                num -= upgradeCost;
+                numClickLabel.Text = num.ToString();
+                upgradeCost += (long)(upgradeCost*0.2);
+                upgradeClickLabel.Text = upgradeCost.ToString();
+                perClick += 1;
+                perClickLabel.Text = perClick.ToString();
+            }
+        }
+
+        private void autoClickButton_Click(object sender, EventArgs e)
+        {
+            //See upgradeButton_Click comment.
+            if (num >= upgradeAutoCost)
+            {
+                num -= upgradeAutoCost;
+                numClickLabel.Text = num.ToString();
+                upgradeAutoCost += (upgradeAutoCost/2);
+                upgradeCostAuto.Text = upgradeAutoCost.ToString();
+                currentAuto += 1;
+                currentAutoClick.Text = (currentAuto1+=1).ToString();
+            }
+        }
+
+        private void clickerTimer_Tick(object sender, EventArgs e)
+        {
+            /*The Timer is set on an interval of 100ms. 
+             *Every 100ms, the number of clicks increment by the currentAuto value
+             *The Label is then updated
+            */
+            num += currentAuto;
+            numClickLabel.Text = num.ToString();
+        }
+
+        private void autoClickButton2_Click(object sender, EventArgs e)
+        {
+            //See upgradeButton_Click comment.
+            if (num >= currentAuto2Cost)
+            {
+                num -= currentAuto2Cost;
+                numClickLabel.Text = num.ToString();
+                currentAuto2Cost += (currentAuto2Cost/2);
+                autoClick2Cost.Text = currentAuto2Cost.ToString();
+                currentAuto += 100;
+                autoClick2Current.Text = (currentAuto2 += 100).ToString();
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            /*The save is written using most basic TextWriter
+             *It saves the value into a text file named status.txt line by line
+             *Note: This is very insecure since you can modify the text file easily 
+            */
+            TextWriter tw = new StreamWriter("status.txt");
+
+            tw.WriteLine(perClick);
+            tw.WriteLine(num);
+            tw.WriteLine(upgradeCost);
+            tw.WriteLine(upgradeAutoCost);
+            tw.WriteLine(currentAuto);
+            tw.WriteLine(currentAuto2);
+            tw.WriteLine(currentAuto2Cost);
+            tw.WriteLine(currentAuto1);
+            tw.WriteLine(auto3Cost);
+            tw.WriteLine(auto3Current);
+
+            tw.Close();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            /*The load button loads the status.txt using TextReader
+             *It loads the value line by line and updates them into the variables.
+            */
+            try
+            {
+                TextReader tr = new StreamReader("status.txt");
+
+                perClick = long.Parse(tr.ReadLine());
+                num = long.Parse(tr.ReadLine());
+                upgradeCost = long.Parse(tr.ReadLine());
+                upgradeAutoCost = long.Parse(tr.ReadLine());
+                currentAuto = long.Parse(tr.ReadLine());
+                currentAuto2 = long.Parse(tr.ReadLine());
+                currentAuto2Cost = long.Parse(tr.ReadLine());
+                currentAuto1 = long.Parse(tr.ReadLine());
+                auto3Cost = long.Parse(tr.ReadLine());
+                auto3Current = long.Parse(tr.ReadLine());
+
+                numClickLabel.Text = num.ToString();
+                upgradeClickLabel.Text = upgradeCost.ToString();
+                perClickLabel.Text = perClick.ToString();
+                upgradeCostAuto.Text = upgradeAutoCost.ToString();
+                currentAutoClick.Text = currentAuto1.ToString();
+                autoClick2Cost.Text = currentAuto2Cost.ToString();
+                autoClick2Current.Text = currentAuto2.ToString();
+                Auto3Cost.Text = auto3Cost.ToString();
+                Auto3Current.Text = auto3Current.ToString();
+
+                tr.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Auto3Button_Click(object sender, EventArgs e)
+        {
+            //See upgradeButton_Click comment.
+            if (num >= auto3Cost)
+            {
+                num -= auto3Cost;
+                numClickLabel.Text = num.ToString();
+                auto3Cost += (auto3Cost / 2);
+                Auto3Cost.Text = auto3Cost.ToString();
+                currentAuto += 10000;
+                Auto3Current.Text = (auto3Current += 10000).ToString();
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            /* Reset button sets each of the value to its default
+             * then it updates the labels.
+            */
+            perClick = 1;
+            num = 0;
+            upgradeCost = 20;
+            upgradeAutoCost = 100;
+            currentAuto = 0;
+            currentAuto2 = 0;
+            currentAuto2Cost = 10000;
+            currentAuto1 = 0;
+            auto3Cost = 10000000;
+            auto3Current = 0;
+
+            numClickLabel.Text = 0.ToString();
+            upgradeClickLabel.Text = 20.ToString();
+            perClickLabel.Text = 1.ToString();
+            upgradeCostAuto.Text = 100.ToString();
+            currentAutoClick.Text = 0.ToString();
+            autoClick2Cost.Text = 10000.ToString();
+            autoClick2Current.Text = 0.ToString();
+            Auto3Cost.Text = 10000000.ToString();
+            Auto3Current.Text = 0.ToString();
+        }
+
+        
+    }
+}
